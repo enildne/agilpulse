b48164a35e3c6d8ab57d6e95c7edf34309396117
zest.kor <zest.kor@b5a67afa-1c85-cad2-13df-3063a5d37f7f>
2010-06-22 17:01:56 +0000

    장비 연결 -> 설정 적용 -> 그래프 획득 까지 구현된 사항 Commit.

--- PulseDisplay/PulseDisplay/DrawRect.h
+++ PulseDisplay/PulseDisplay/DrawRect.h
@@ -11,17 +11,18 @@ public:
 	CDrawRect();
 	virtual ~CDrawRect();
 
-	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
-	afx_msg void OnPaint();
-	afx_msg void OnShowWindow(BOOL bShow, UINT nStatus);
+	afx_msg int		OnCreate(LPCREATESTRUCT lpCreateStruct);
+	afx_msg void	OnPaint();
+	afx_msg void	OnShowWindow(BOOL bShow, UINT nStatus);
 
-	void setLoading(BOOL set) {m_bLoading = set;}
+	void		setPulseData(unsigned char* data);
+	void		setGraphDraw(BOOL set) {m_bLoading = set;}		// FALSE 이면 GRID만 그린다.
 
 protected:
 	DECLARE_MESSAGE_MAP()
-	double		m_dconvData[VALUE_COUNT + 1][2];
 	BOOL		m_bLoading;	
 	double		m_dMinVal, m_dMaxVal;
+	double		m_dconvData[VALUE_COUNT + 1];
 };
 
 
